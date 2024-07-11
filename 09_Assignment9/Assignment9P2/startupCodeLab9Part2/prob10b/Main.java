0a00d8310aa90e47c42b268201dbe6cfc68a5ae3
Madgy Edwar <70660386+Magdyedwar1996@users.noreply.github.com>
2024-07-11 18:30:13 -0500

    this is the assignment number 9 part two

---
+++ 09_Assignment9/Assignment9P2/startupCodeLab9Part2/prob10b/Main.java
@@ -0,0 +1,11 @@
+package lesson9.labs.prob10b;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> List = java.util.List.of(new String[]{"Bill", "Thomas", "Mary"});
+        System.out.println(List.stream().reduce((x,y)-> x+", "+y).orElse(""));
+    }
+}
